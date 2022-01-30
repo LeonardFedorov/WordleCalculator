3d18d83bfabbd0ecd70b7fcd2764e8956ca20979
LeonardFedorov <oliver.ingamells@gmail.com>
2022-01-30 18:14:22 +0000

    First build of word / clue tester

--- WordleCode.fs
+++ WordleCode.fs
@@ -3,6 +3,23 @@ module WordleCode
 
 open System
 
+type WordleClue =
+    | Green of char * int // char * position
+    | Yellow of char * (int list) * int // char * (list of yellow/grey positions of char) * count of G/Y clues for char
+    | Grey of char * int // char * count of G/Y clues for char
 
+let countChar char (word: string) =
+    Array.fold (fun s c -> if c = char then s + 1 else s) 0 (word.ToCharArray())
 
+let applyClue (word: string) clue =
+    match clue with
+        | Green(c, i) -> if word.[i] = c then true else false
+        | Grey(c, n) -> if countChar c word = n then true else false
+        | Yellow(c, posList, n) -> if
+                                      List.fold (fun s i -> word.[i] <> c && s) true posList && 
+                                      countChar c word >= n
+                                   then
+                                      true
+                                   else 
+                                      false
 
