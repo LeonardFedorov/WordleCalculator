ac273ebea2b5ada13bcf1a52f85a3feb131cdc1d
LeonardFedorov <oliver.ingamells@gmail.com>
2022-01-30 23:26:27 +0000

    Built out other core Wordle functions

--- WordleCode.fs
+++ WordleCode.fs
@@ -2,6 +2,7 @@
 module WordleCode
 
 open System
+open System.IO
 
 type WordleClue =
     | Green of char * int // char * position
@@ -11,6 +12,7 @@ type WordleClue =
 let countChar char (word: string) =
     Array.fold (fun s c -> if c = char then s + 1 else s) 0 (word.ToCharArray())
 
+//Tests if word is consistent with the given clue
 let applyClue (word: string) clue =
     match clue with
         | Green(c, i) -> if word.[i] = c then true else false
@@ -23,3 +25,21 @@ let applyClue (word: string) clue =
                                    else 
                                       false
 
+
+//Tests if a word is consistent with all of a given list of clues
+let testWord (word: string) (clues: WordleClue list) =
+    List.map (fun clue -> applyClue word clue ) clues
+    |> List.exists (fun x -> x = false)
+    |> not
+
+//Filter a list of words to only those that satisfy all of the clues
+let filterList (wordList: string[]) (clues: WordleClue list) =
+    Array.filter (fun word -> testWord word clues) wordList
+
+let printWordList (wordList: string[]) =
+    let (output: string) = String.concat ", " wordList
+    Console.WriteLine output
+
+let importWordList targetDir =
+    let fileStream = new StreamReader(targetDir + "\\WordList.txt")
+    fileStream.ReadToEnd().Split([|"\r\n"|], StringSplitOptions.RemoveEmptyEntries)
