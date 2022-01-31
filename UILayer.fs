44e9f1e1b535b3908fb628da4d27964df2dd630e
LeonardFedorov <oliver.ingamells@gmail.com>
2022-01-31 23:45:01 +0000

    First functional build

--- UILayer.fs
+++ UILayer.fs
@@ -1,12 +1,74 @@
 ﻿open System
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+//Start a new wordle session
+let rec RunSession targetDir =
 
+    let rec SessionIter wordList =
+
+        //Functions to test the validity of inputs
+        let validWord (word: string) =
+            if word.Length <> WordleCode.stringLength then false
+            elif Array.tryFind (fun c -> int c < 97 || int c > 122) (word.ToCharArray()) <> None then false
+            else true
+
+        let validClues (clues: string) =
+            if clues.Length <> WordleCode.stringLength then false
+            elif Array.tryFind (fun c -> c <> 'g' && c <> 'y' && c <> '-') (clues.ToCharArray()) <> None then false
+            else true
+
+        //Get the next guess and clue result from the user
+        Console.WriteLine("Input guess (all lower case):")
+        let guess = Console.ReadLine()
+        if not (validWord guess) then failwith "Word input invalid"
+
+        Console.WriteLine("Input clue string (g = Green, y = Yellow, - = Grey:")
+        let clueString = Console.ReadLine()
+        if not (validClues clueString) then failwith "Clue input invalid"
+
+        let revisedList = WordleCode.parseClueList guess clueString
+                          |> WordleCode.filterList wordList
+        Console.WriteLine("\nRevised list contains " + (Array.length revisedList).ToString() + " words.")
+
+        let getNextStep =
+            let rec getNextStepIter (nextStep: string) = 
+                
+                match nextStep.[0] with
+                    | 'G' | 'g' -> nextStep.[0]
+                    | 'P' | 'p' -> WordleCode.printWordList revisedList
+                                   Console.WriteLine("(G)uess another word, (P)rint valid word list, (R)estart or (E)nd?")
+                                   getNextStepIter (Console.ReadLine())
+                    | 'R' | 'r' -> nextStep.[0]
+                    | 'E' | 'e' -> nextStep.[0]
+                    | _ -> Console.WriteLine("Invalid input, please input again:")
+                           getNextStepIter (Console.ReadLine())
+
+            Console.WriteLine("(G)uess another word, (P)rint valid word list, (R)estart or (E)nd?")
+            getNextStepIter (Console.ReadLine())
+            
+        let nextStep = getNextStep
+        match nextStep with
+            | 'G' | 'g' -> SessionIter revisedList
+            | 'R' | 'r' -> RunSession targetDir
+            | 'E' | 'e' -> ()
+            | _ -> failwith "Unknown user response parsed"
+
+        Console.Write("\n")
+
+        ()
+
+    //Import the fresh word list and then start the iterator
+    Console.WriteLine("Starting New Session")
+
+    let wordList = WordleCode.importWordList targetDir
+    Console.WriteLine("Word List Imported with " + (Array.length wordList).ToString() + " words\n")
+
+    SessionIter wordList
+    
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    Console.WriteLine("Wordle Simulator")
+    let currentDir = Environment.CurrentDirectory.ToString()
+    RunSession currentDir
+
     0 // return an integer exit code
--- WordleCode.fs
+++ WordleCode.fs
@@ -4,16 +4,45 @@ module WordleCode
 open System
 open System.IO
 
+let stringLength = 5
+
 type WordleClue =
     | Green of char * int // char * position
     | Yellow of char * (int list) * int // char * (list of yellow/grey positions of char) * count of G/Y clues for char
     | Grey of char * int // char * count of G/Y clues for char
 
-let countChar char (word: string) =
-    Array.fold (fun s c -> if c = char then s + 1 else s) 0 (word.ToCharArray())
+//Parses the guessed word and the returned clue string into a list of WordleClues
+let parseClueList (word: string) (clueString: string) =
+
+    let parseClueLetter (clueIndex: int) =
+ 
+        let countGYs (targetChar: char) =
+            Array.fold2 (fun count char clue -> if char = targetChar && (clue = 'g' || clue = 'y') then count + 1 else count) 0 (word.ToCharArray()) (clueString.ToCharArray())
+         
+        let findYGreys (targetChar: char) =
+            let mutable indexList = List.Empty
+            for i in 0 .. (stringLength - 1) do
+                if word.[i] = targetChar && (clueString.[i] = 'y' || clueString.[i] = '-') then indexList <- i :: indexList else ()
+            indexList
+ 
+        let currentClue = clueString.[clueIndex]
+        let currentChar = word.[clueIndex]
+        match currentClue with
+            | 'g' -> Green(currentChar, clueIndex)
+            | 'y' -> Yellow (currentChar, findYGreys currentChar, countGYs currentChar)
+            | '-' -> Grey(currentChar, countGYs currentChar)
+            | _ -> failwith "Unexpected clue type"    
+            
+    List.init stringLength (fun i -> parseClueLetter i)
+    |> List.distinct //Greys and Yellows can create repeated clues, so trim these to keep the filter efficient
+    
 
 //Tests if word is consistent with the given clue
 let applyClue (word: string) clue =
+
+    let countChar char (word: string) =
+        Array.fold (fun s c -> if c = char then s + 1 else s) 0 (word.ToCharArray())
+
     match clue with
         | Green(c, i) -> if word.[i] = c then true else false
         | Grey(c, n) -> if countChar c word = n then true else false
@@ -28,7 +57,7 @@ let applyClue (word: string) clue =
 
 //Tests if a word is consistent with all of a given list of clues
 let testWord (word: string) (clues: WordleClue list) =
-    List.map (fun clue -> applyClue word clue ) clues
+    List.map (fun clue -> applyClue word clue) clues
     |> List.exists (fun x -> x = false)
     |> not
 
@@ -36,9 +65,12 @@ let testWord (word: string) (clues: WordleClue list) =
 let filterList (wordList: string[]) (clues: WordleClue list) =
     Array.filter (fun word -> testWord word clues) wordList
 
+
+//Miscellaneous functions, mostly I/O
 let printWordList (wordList: string[]) =
     let (output: string) = String.concat ", " wordList
     Console.WriteLine output
+    Console.Write ("\n")
 
 let importWordList targetDir =
     let fileStream = new StreamReader(targetDir + "\\WordList.txt")
